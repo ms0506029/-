16de9c75021b6aa882f3ba5d05c8ce37ef88cc5d
ms0506029 <eddc9104@gmail.com>
2025-10-03 00:43:59 +0900

    Update health.js

--- api/health.js
+++ api/health.js
@@ -1,7 +1,3 @@
-import withCors from './_lib/cors';
-
-function handler(req, res) {
+export default (req, res) => {
   res.status(200).json({ ok: true, time: new Date().toISOString() });
-}
-
-export default withCors(handler);
+};
