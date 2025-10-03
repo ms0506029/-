5d6ee323877845fbbb340b2b82da81505ab7d233
ms0506029 <eddc9104@gmail.com>
2025-10-03 13:04:07 +0900

    Update health.js

--- api/health.js
+++ api/health.js
@@ -1,3 +1,3 @@
-export default (req, res) => {
-  res.status(200).json({ ok: true, time: new Date().toISOString() });
+module.exports = (req, res) => {
+res.status(200).json({ ok: true, time: new Date().toISOString() });
 };
